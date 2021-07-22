1c88a59f29afafa528d730dfad3a634513bb36db
Denys Nikitin <nikitin.denys@pm.me>
2021-07-22 09:53:37 +0300

    Setup React + Typescript project with MaterialUI and MobX

---
+++ src/client/src/App.tsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from "@material-ui/core";
+
+export default function App() {
+    return (
+        <>
+            <Box p={4}>
+                <Typography variant="h1">
+                    Hello World
+                </Typography>
+            </Box>
+        </>
+    );
+}
